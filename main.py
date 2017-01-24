c46cc33388c4cfe4e6a84b0fbc296fd8bb864622
Zhenbo Li <litimetal@gmail.com>
2017-01-24 18:39:57 +0800

    construct loop

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ SHOP_LINKS = ["https://shop62237807.taobao.com", "https://shop65188790.taobao.co
 TEMP_FOLDER = "/Users/lizhenbo/Downloads/"
 TEMP_HTML_PAGE = TEMP_FOLDER + "index.html"
 
+class Item(object):
+    __slots__ = 'card', 'shop_link', 'search_link', 'html'
+
 import selenium_browser
+
+for c in CARD_NAMES:
+    for s in SHOP_LINKS:
+        i = Item()
+        i.card = c
+        i.shop_link = s
+        i.search_link = SHOP_LINKS[0]
+        i.html = selenium_browser.fetch(i.search_link)
 
 html = selenium_browser.fetch(SHOP_LINKS[0])
 
