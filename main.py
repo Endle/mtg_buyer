ceb837b129fc8c6eca96dcf3ddb9552adb099061
Zhenbo Li <litimetal@gmail.com>
2017-01-24 18:16:08 +0800

    rename to selenium_browser

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ SHOP_LINKS = ["https://shop62237807.taobao.com", "https://shop65188790.taobao.co
 TEMP_FOLDER = "/Users/lizhenbo/Downloads/"
 TEMP_HTML_PAGE = TEMP_FOLDER + "index.html"
 
-import browser
+import selenium_browser
 
-html = browser.fetch(SHOP_LINKS[0])
+html = selenium_browser.fetch(SHOP_LINKS[0])
 
 with open(TEMP_HTML_PAGE, "w") as fout:
     fout.write(html)
 
-browser.clean_up_before_quit()
+selenium_browser.clean_up_before_quit()
