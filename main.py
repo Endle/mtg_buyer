3f72c02caaba68950f75eea10101e118fad8ed02
Zhenbo Li <litimetal@gmail.com>
2017-01-24 18:12:49 +0800

    save temp page

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 CARD_NAMES = ["背心", "文胸", "打底"]
 SHOP_LINKS = ["https://shop62237807.taobao.com", "https://shop65188790.taobao.com"]
 
-TEMP_FOLDER = "/tmp/mtg"
+TEMP_FOLDER = "/Users/lizhenbo/Downloads/"
+TEMP_HTML_PAGE = TEMP_FOLDER + "index.html"
 
 import browser
 
-browser.fetch("http://www.baidu.com")
+html = browser.fetch(SHOP_LINKS[0])
+
+with open(TEMP_HTML_PAGE, "w") as fout:
+    fout.write(html)
 
 browser.clean_up_before_quit()
