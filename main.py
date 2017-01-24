ca8bd834e5d92846c3179a8fea481877c3b04603
Zhenbo Li <litimetal@gmail.com>
2017-01-24 19:59:47 +0800

    Construct search link

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+class Item(object):
+    __slots__ = 'card', 'shop_link', 'search_link', 'html'
+
+import urllib.parse
+import time
+def _get_url(item:Item)->str:
+    url_component = [
+        item.shop_link,
+        "/search.htm?q=",
+        urllib.parse.quote_plus(item.card.encode('gbk')),
+        "&searcy_type=item",
+        "&s_from=newHeader&source=&ssid=s5-e&search=y",
+        "&viewType=list",
+        "&initiative_id=shopz_",
+        time.strftime("%Y%m%d"),
+    ]
+    return "".join(url_component)
+
 CARD_NAMES = ["背心", "文胸", "打底"]
 SHOP_LINKS = ["https://shop62237807.taobao.com", "https://shop65188790.taobao.com"]
 
 TEMP_FOLDER = "/Users/lizhenbo/Downloads/"
 TEMP_HTML_PAGE = TEMP_FOLDER + "index.html"
-
-class Item(object):
-    __slots__ = 'card', 'shop_link', 'search_link', 'html'
 
 import selenium_browser
 
@@ -14,7 +29,7 @@ for c in CARD_NAMES:
         i = Item()
         i.card = c
         i.shop_link = s
-        i.search_link = SHOP_LINKS[0]
+        i.search_link = _get_url(i)
         i.html = selenium_browser.fetch(i.search_link)
 
 html = selenium_browser.fetch(SHOP_LINKS[0])
